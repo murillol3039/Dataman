f95cb5fbf1f8671bf9f0442676045d38b358e02c
Edmund <40037900+FreedomGuard224@users.noreply.github.com>
2019-11-11 12:48:05 -0500

    Update popQuiz.py

--- PopQuiz/popQuiz.py
+++ PopQuiz/popQuiz.py
@@ -9,13 +9,19 @@ problems = []
 for row in reader:
     try:
         problems.append([ int(row[0]), row[1], int(row[2]) ])
+        firstNum = problems[0]
+        print(firstNum)
+        for item in problems:
+            print(item)
+            test = int(input("Pause"))
+            
     except:
         pass
+    print("done")
 
 print("Here are the problems from the file: ")
 
-for item in problems:
-    print(item)
+
 
 
 
