d405655f8baf69a2a59d1c21be224021f9388901
FreedomGuard224 <FreedomGuard224@users.noreply.github.com>
2019-11-16 19:26:57 -0500

    changes to menu and number guesser

--- menu.py
+++ menu.py
@@ -4,26 +4,36 @@ import memoryBank as mb
 import popQuiz as pq
 
 def main():
-    print("1) Answer checker")
-    print("2) Memory bank")
-    print("3) Number guesser")
-    print("4) Pop Quiz")
-    print("5) Exit")
-    choice = int(input("Pick an option: "))
+    try:
+        print("1) Answer checker")
+        print("2) Memory bank")
+        print("3) Number guesser")
+        print("4) Pop Quiz")
+        print("5) Exit")
+        choice = int(input("Pick an option: "))
 
-    if choice == 1:
-          ac.mainMenu()
+        while choice <1 or choice >5:
+            choice = int(input("Pick an option 1-5: "))
+    
+        if choice == 1:
+              ac.mainMenu()
 
-    elif choice == 2:
-          mb.memoryBankMenu()
+        elif choice == 2:
+              mb.memoryBankMenu()
 
-    elif choice == 3:
-          ng.main()
+        elif choice == 3:
+              ng.main()
 
-    elif choice == 4:
-        pq.main()
+        elif choice == 4:
+            pq.main()
 
-    else:
-          print("exit")
+        else:
+              print("exit")
+              
+    except ValueError:
+        print("Error: Data must be a number.")
+        main()
+        print("")
+    main()
 
 main()
